2b66943f9546adbd9d7534e1e13bc698b394299b
Kislay <28885631+codeprogredire@users.noreply.github.com>
2024-04-05 09:31:24 +0530

    added image to about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
 
+const user={
+    name: 'Shadow',
+    imageUrl: '/src/assets/shadow.jpg',
+    imageSize:300
+};
+
 export function AboutPage(){
     return (
         <div className='about-page'>
             <center>
                 <h1><i>About me page</i></h1>
-                <p>Hello there. <br/>How do you do?</p>
+                <h1>{user.name}</h1>
+                <img
+                    className="avatar"
+                    src={user.imageUrl}
+                    alt={'Photo of ' + user.name}
+                    style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                    }}></img>
+                    <br /><br />
                 <Link to='/'>Go to homepage</Link>
             </center>
         </div>
